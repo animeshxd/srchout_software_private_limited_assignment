2777fb9bc67c11bbe6445b908131c7c5f138393d
Animesh Murmu <am2646374@gmail.com>
2023-10-28 18:51:48 +0530

    Add basic nevigator page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:srchout_software_private_limited_assignment/view/bike_details/bike_details_view.dart';
+import 'package:srchout_software_private_limited_assignment/view/home/home_view.dart';
 
 import 'constrains.dart';
 import 'view/checkout/check_out_view.dart';
@@ -26,7 +28,42 @@ class MainApp extends StatelessWidget {
           backgroundColor: AppTheme.mainColor,
         ),
       ),
-      home: const CheckOutView(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyAppView(),
+        '/home': (context) => const HomeView(),
+        '/checkout': (context) => const CheckOutView(),
+        '/details': (context) => const BikeDetailsView(),
+      },
+    );
+  }
+}
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pushNamed('/home'),
+              child: const Text('/home'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pushNamed('/checkout'),
+              child: const Text('/checkout'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pushNamed('/details'),
+              child: const Text('/details'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
